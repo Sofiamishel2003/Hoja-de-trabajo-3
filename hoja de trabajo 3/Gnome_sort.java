e9a770bc6c9c175f5bb382a72bcbfc379e3fc011
Sofía Velásquez <98661058+Sofiamishel2003@users.noreply.github.com>
2023-02-07 20:35:56 -0600

    Actualización del show

--- hoja de trabajo 3/Gnome_sort.java
+++ hoja de trabajo 3/Gnome_sort.java
@@ -18,11 +18,16 @@ public class Gnome_sort {
         }
         return sort_arr;
     }
-    public void show(int[] arr)
+   public String show(int[] arr)
     {
         int n = arr.length;
+        String text="";
         for (int i = 0; i < n; ++i)
+        {
             System.out.print(arr[i] + " ");
+            text = text + arr[i] + " ";
+        }
         System.out.println();
+        return  text;
     }
 }
